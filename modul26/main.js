b224d486cba933e3a4e6d249179e850bd0b2f6ca
patris491 <patrismaxhuni1@gmail.com>
2026-03-05 17:41:12 +0100

    update

--- modul26/main.js
+++ modul26/main.js
@@ -1,1 +1,72 @@
-console.log('helooooo')
+console.log('helooooo');
+
+var x = 10;
+
+var value="digitalschool";
+
+var y = 50;
+
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+
+var a = 99
+
+console.log(a)
+
+a++;
+
+console.log(a)
+
+var b = 201
+
+b--;
+
+console.log(b)
+
+var c =115;
+
+c+=5;
+
+console.log(c)
+
+
+var d =115;
+
+d-=5;
+
+console.log(d)
+
+
+
+var v =115;
+
+v*=5;
+
+console.log(v)
+
+
+var f=115;
+f/=5;
+
+console.log(f)
+
+
+var e = 100;
+
+var w = 10;
+
+console.log(e == w)
+console.log(e != w)
+console.log(e < w)
+console.log(e > w)
+
+var emri="hitler"
+var mbiemri="maxhuni"
+
+var fullName = emri+" "+mbiemri
+
+console.log(fullName)
+
+console.log(`HALOOO from ${emri}`)

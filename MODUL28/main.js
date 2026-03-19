a1e19e6555a60050746645db42e77ad053ad4e2f
patris491 <patrismaxhuni1@gmail.com>
2026-03-19 17:46:06 +0100

    moduli28

--- MODUL28/main.js
+++ MODUL28/main.js
@@ -20,14 +20,39 @@ btn3.onmouseleave = function() {
 var btn4 = document.getElementById("btn4");
 
 btn4.onwheel = function() {
+};
+
+var button1 = document.getElementById("btn6");
+var text2 = document.getElementById("school");
+
+var button2 = document.getElementById("btn7");
+var text3 = document.getElementById("schoole");
+
+button1.onclick = function() {
+    text2.style.color = "blue";
+    text2.style.backgroundColor = "yellow";
+    text2.style.padding = "20px";
+    text2.style.textAlign = "center";
+    text2.style.fontSize = "100px";
     alert("Button 4 wheel!");
 };
 
-var btn5 = document.getElementById("btn5");
-
-var text1 = document.getElementById("text1");
+= document.getElementById("text1");
 
 btn5.onclick = function() {
     text1.style.color = "red";
-};
+}var btn5 = document.getElementById("btn5");
+
+var text1 
+
+button2.onclick = function() {
+    text3.style.cssText = "color: green; background-color: pink; padding: 20px; text-align: center; font-size: 25px; font-family: fantasy;";
+}
+
+var button3 = document.getElementById("ifundit");
+var text4 = document.getElementById("pg");
+
+button3.onclick = function() {
+    text4.setAttribute("class", "paragraf");
+}
 

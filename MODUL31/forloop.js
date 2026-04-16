eadb3c734ed70ebb0e139f5bd9347062ea4293d0
patris491 <patrismaxhuni1@gmail.com>
2026-04-16 17:42:56 +0200

    modul31

---
+++ MODUL31/forloop.js
@@ -0,0 +1,30 @@
+var cars = ['volvo','bmw','audi','ford','mazda','toyota']
+
+var text = '';
+
+
+// text +=  cars[0] + '<br>';
+// text +=  cars[1] + '<br>';
+// text +=  cars[2] + '<br>';
+// text +=  cars[3] + '<br>';
+// text +=  cars[4] + '<br>';
+// text +=  cars[5] + '<br>';
+
+// document.getElementById('forloop1').innerHTML = text;
+// console.log(text);
+
+for(i=0; i < cars.length; i++) {
+    text += cars[i] + '<br>';
+}
+
+document.getElementById('forloop2').innerHTML = text;
+console.log(text);
+
+var text1
+
+for(x = 0; x <= 5; x++){
+    text1 +="the number is " + x + "<br>";
+}
+
+document.getElementById('forloop3').innerHTML = text1;
+console.log(text1);

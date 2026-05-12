231d2826b2819f1225275f8a85eed518411f58bc
patris491 <patrismaxhuni1@gmail.com>
2026-05-12 17:49:22 +0200

    modul36

---
+++ MODULI36/main.js
@@ -0,0 +1,11 @@
+// $(window).ready(function(){
+//     alert("It is working")
+// })
+
+// // $("#button").click(function(){
+// //       console.log("the testing has been successfully!")
+// })
+
+$("#button").click(function(){
+    $("div").addClass("clicked")
+}).find('span').attr('title','hover over me')
